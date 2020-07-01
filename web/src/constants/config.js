1fdb27a7ba7d45b9e5f547a7ee98fa4fd0a38f05
SagredoIsmael <sagredo.ismael@live.com>
2020-07-02 00:41:45 +0200

    add config firebase for web

---
+++ web/src/constants/config.js
@@ -0,0 +1,11 @@
+export default {
+    FIREBASE: {
+        apiKey: "AIzaSyAvgp3bsOy_83mOBgnGxn76-v-km97Cdio",
+        authDomain: "myrealfoodtech-2390b.firebaseapp.com",
+        databaseURL: "https://myrealfoodtech-2390b.firebaseio.com",
+        projectId: "myrealfoodtech-2390b",
+        storageBucket: "myrealfoodtech-2390b.appspot.com",
+        messagingSenderId: "31827033524",
+        appId: "1:31827033524:web:22a81bbc34b7b94ee186e1"
+    },
+}
