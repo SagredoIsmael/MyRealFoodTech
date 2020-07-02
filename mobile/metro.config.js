b6e60028608831505e0ff60315d43c8be8ac029e
SagredoIsmael <sagredo.ismael@live.com>
2020-07-02 13:43:33 +0200

    modify metro config for common access

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -1,10 +1,10 @@
-/**
- * Metro configuration for React Native
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
+const path = require('path')
+const extraNodeModules = {
+  'common': path.resolve(__dirname + '/../common'),
+}
+const watchFolders = [
+  path.resolve(__dirname + '/../common')
+]
 module.exports = {
   transformer: {
     getTransformOptions: async () => ({
@@ -13,5 +13,9 @@ module.exports = {
         inlineRequires: false,
       },
     }),
+  }, 
+  resolver: {
+    extraNodeModules
   },
-};
+  watchFolders,
+}
