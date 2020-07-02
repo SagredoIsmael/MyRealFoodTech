d5e72b6b380096f7c680359f6a44d8365422d431
SagredoIsmael <sagredo.ismael@live.com>
2020-07-02 13:54:46 +0200

    add common package to web

--- web/src/App.js
+++ web/src/App.js
@@ -1,15 +1,16 @@
 import * as React from 'react'
 import firebase from "firebase"
-import constant from './constants/config'
+import config from './constants/config'
+import colors from 'common/constants/colors'
 require("firebase/firestore")
 
-firebase.initializeApp(constant.FIREBASE)
+firebase.initializeApp(config.FIREBASE)
 
 export const firestore = firebase.firestore()
 
 export default () => {
   return (
-    <div style={{ flex: 1, height: '4rem', width: '4rem', backgroundColor: 'red' }}>
+    <div style={{ flex: 1, height: '4rem', width: '4rem', backgroundColor: colors.primary }}>
 
     </div>
   )
