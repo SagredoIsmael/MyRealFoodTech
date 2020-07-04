4970469302fb823d2e6880e725225af4039811d2
SagredoIsmael <sagredo.ismael@live.com>
2020-07-04 18:58:38 +0200

    add redux config to web

--- packages/web/src/App.js
+++ packages/web/src/App.js
@@ -2,6 +2,9 @@ import * as React from 'react'
 import firebase from "firebase"
 import config from './constants/config'
 import colors from 'commons/src/constants/colors'
+import { Provider } from 'react-redux'
+import { store } from './redux/configStore'
+
 require("firebase/firestore")
 
 firebase.initializeApp(config.FIREBASE)
@@ -9,9 +12,9 @@ firebase.initializeApp(config.FIREBASE)
 export const firestore = firebase.firestore()
 
 export default () =>
-<div style={{ flex: 1, height: '4rem', width: '4rem', backgroundColor: colors.primary }}> 
+    <Provider store={store}>
+        <div style={{ flex: 1, height: '4rem', width: '4rem', backgroundColor: colors.primary }}/>
+    </Provider>
 
-</div>
 
 
-  
