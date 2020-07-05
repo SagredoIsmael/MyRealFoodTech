baa50338078074713177ccf170b18850ca7c2724
SagredoIsmael <sagredo.ismael@live.com>
2020-07-05 12:58:20 +0200

    add navigation config to mobile

--- packages/common/src/constants/colors.js
+++ packages/common/src/constants/colors.js
@@ -1,5 +1,7 @@
 export default {
     primary: '#b6e044',
     secondary: '#19755b',
-    black: '#131313'
+    black: '#131313',
+    white: 'white',
+    grey: 'grey'
 }
---
+++ packages/common/src/constants/navigation.js
@@ -0,0 +1,7 @@
+export const screens = {
+    SIGN: 'Sign',
+    MAIN: 'Main',
+    MYTODOS: 'My Todos',
+    OTHERSTODOS: 'Other Todos',
+    CREATETODO: 'Create Todo'
+}
